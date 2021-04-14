44a86c6aece585d85e73332d83f526561f9142b0
craldo <crizgiraldo@gmail.com>
2021-04-14 04:59:33 -0500

    modificacion de regiones

--- prereq/aws-codepipeline-prereq.sh
+++ prereq/aws-codepipeline-prereq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws cloudformation deploy \
---region us-east-1 \
+--region us-west-2 \
 --stack-name codepipeline-lambda-prereq \
 --template-file ./aws-codepipeline-prereq.yml \
 --capabilities CAPABILITY_IAM \
