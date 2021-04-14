64ab06f398b076303bbcfc8a876e5afe49f126b2
craldo <51692172+kriztiang@users.noreply.github.com>
2021-04-14 08:03:09 -0500

    modificacion Creacion Dynamo

--- dynamo/dynamo.sh
+++ dynamo/dynamo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws cloudformation deploy \
 --region us-west-2 \
---parameter-overrides DynamoName="lablambda" DynamoKey="dni" \
+--parameter-overrides DynamoName="DynamoPlatzi" DynamoKey="cc" \
 --stack-name dynamo-lambda-lab \
 --template-file ./dynamo.yml
