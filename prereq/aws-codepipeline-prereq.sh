a90980d94b9c260df7086be025c72fe48e4000b6
dupegui334 <davidupegui334@hotmail.com>
2022-09-22 02:13:44 +0000

    Added .gitignore file

--- prereq/aws-codepipeline-prereq.sh
+++ prereq/aws-codepipeline-prereq.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-aws cloudformation deploy \
---region us-east-1 \
---stack-name codepipeline-lambda-prereq \
---template-file ./aws-codepipeline-prereq.yml \
---capabilities CAPABILITY_IAM \
---capabilities CAPABILITY_NAMED_IAM
+# aws cloudformation deploy \
+# --region us-east-1 \ 
+# --stack-name labpreq \ 
+# --template-file ./aws-codepipeline-prereq2.yml \
+# --capabilities CAPABILITY_IAM \
+# --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --region us-east-1 --stack-name labpreq --template-file ./aws-codepipeline-prereq.yml --capabilities CAPABILITY_IAM --capabilities CAPABILITY_NAMED_IAM
