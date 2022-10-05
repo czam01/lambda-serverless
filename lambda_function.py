498e9d6a0458dc6ce93f297cb9e6bd5eea27c3fc
Carlos Zambrano <czam01@gmail.com>
2022-10-04 19:43:07 -0500

    Update lambda_function.py
    hola soy un commit!

--- lambda_function.py
+++ lambda_function.py
@@ -4,20 +4,17 @@ from boto3.dynamodb.conditions import Key
 
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
-# Estamos en CloudCamp con el mejor Profe 19 Sep
-
-
 class DynamoAccessor:
     def __init__(self, dynamo_table):
         dynamo_db = boto3.resource('dynamodb')
         self.table = dynamo_db.Table(dynamo_table)
 
-    def get_data_from_dynamo(self, cc):
-        response = self.table.query(KeyConditionExpression=Key('cc').eq(cc))
+    def get_data_from_dynamo(self, dni):
+        response = self.table.query(KeyConditionExpression=Key('dni').eq(dni))
         return response["Items"][0] if any(response["Items"]) else None
 
 def lambda_handler(event, context):
     dynamo_backend = DynamoAccessor(DYNAMO_BD)
-    db_element = dynamo_backend.get_data_from_dynamo(event['cc'])
+    db_element = dynamo_backend.get_data_from_dynamo(event['dni'])
     return db_element
 
